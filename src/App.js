8b90c6d86e3d231bbcd959a15e5e9f2fd5fe6637
Raider Levie <raiderlevie@Raiders-MacBook-Pro.local>
2024-11-06 14:18:21 -0600

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import logo from './logo.svg'; // Update if you use a different logo
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to My React App - Explore More Below!</p>
+          <nav>
+            <Link to="/" className="App-link">Home</Link>
+            <Link to="/about" className="App-link">About</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
