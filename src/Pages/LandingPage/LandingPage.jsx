274a70fe2442a10680cbfa9aded5a08caddcce7f
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-11-06 15:02:03 -0600

    added more

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function LandingPage(){
     return (
         <div>
-            This going to be the landing Page
+            This going to be the landing P,dsjkn,fj.qen.j,ekwbcvjwe,dkchj,wekjhc,wjhage
         </div>
     )
 }
