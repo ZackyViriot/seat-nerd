c3e460a7339867ebb076556e0b9e05ab0dbde1d9
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-11-06 15:01:10 -0600

    removed extra folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import LandingPage from './Pages/Home/LandingPage';
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 
 function App() {
