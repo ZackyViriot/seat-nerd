b4b6306ec47ed7cc111400a7da6bd53464f7c466
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-11-06 15:19:08 -0600

    added structure

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function LandingPage(){
     return (
-        <div>
-            This going to be the landing P,dsjkn,fj.qen.j,ekwbcvjwe,dkchj,wekjhc,wjhage
+        <div className='bg-red-500'>
+            Landing Page
         </div>
     )
 }
