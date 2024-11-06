71da2ada90bfc2c5f6863da5972ef5f23e581a2c
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-11-06 14:39:41 -0600

    added basic for landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.svg'; // Update if you use a different logo
-import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import LandingPage from './Pages/Home/LandingPage';
+
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path = '/' />
+          <Route path = '/' element={<LandingPage/>}/>
         </Routes>
       </BrowserRouter>
   );
---
+++ src/Pages/Home/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function LandingPage(){
+    return (
+        <div>
+            This going to be the landing Page
+        </div>
+    )
+}
